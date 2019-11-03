e101486cf775c51685f461b22ab8152249d511f4
switchback <switchback.572@gmail.com>
2019-11-03 14:13:44 +0900

    5の倍数のときのfizzbuzzを実装

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
 def fizzbuzz_convert(number):
     if number % 3 == 0:
         return "Fizz"
+    if number % 5 == 0:
+        return "Buzz"
+
 
 
 # TODO 〇〇
 assert fizzbuzz_convert(3) == "Fizz"
+assert fizzbuzz_convert(5) == "Buzz"
